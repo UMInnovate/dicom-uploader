aa12dea6eed29d73129aba9f15c78cb575f72665
Lenny Martinez <lennymartinezd@gmail.com>
2020-07-24 15:14:30 -0400

    rewrite index.html page to be neater and cleaner

--- FlaskApp/static/js/scripts.js
+++ FlaskApp/static/js/scripts.js
@@ -1,84 +1,19 @@
-/*!
-    * Start Bootstrap - Grayscale v6.0.1 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-grayscale/blob/master/LICENSE)
-    */
-(function ($) {
-    "use strict"; // Start of use strict
+function toggleStuff() {
+  'use strict';
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-
-    // When submit button is pressed, hide the form and show the loading message
-    $("#enter").click(function () {
-        var f = document.getElementsByTagName("form")[0];
-        if (f.checkValidity()) {
-            $("#form").addClass("d-none");
-            $("#loading").removeClass("d-none");
-        }
-    });
-
-    // we can keep the below function for when we need to add stuff later
-    $(".quantity").change(function () {
-        var num = $(this).val();
-        var elementarray = []
-        for (let index = 0; index < num; index++) {
-            var element = `<dl class="btn btn-secondary" name="model${index}">
-                                <p class="text-white mx-auto mt-2 mb-5">
-                                    Please select files and captions for model ${index}.
-                                </p>
-                                <input type="file" name="files${index}[]" multiple="true" autocomplete="off" required>
-                                <textarea class="form-control" name="caption${index}" placeholder="Please enter caption" required></textarea>
-                            </dl>`
-            elementarray.push(element)
-        }
-        $("#models").empty();
-        $("#models").append(elementarray);
-    });
-
-})(jQuery); // End of use strict
+  const submitButton = document.getElementById('submitButton');
+  const uploadForm = document.getElementById('uploadForm');
+  const loadingSection = document.getElementById('loadingSection');
+  const instructionText = document.getElementById('instructionText');
+  submitButton.addEventListener('click', toggleLoading);
+  function toggleLoading() {
+    console.log('test');
+    if (uploadForm.checkValidity()) {
+      uploadForm.classList.add('d-none');
+      loadingSection.classList.remove('d-none');
+      instructionText.innerHTML =
+        'Processing your files. Please do not click the back button or refresh the page.';
+    }
+  }
+}
+toggleStuff();
